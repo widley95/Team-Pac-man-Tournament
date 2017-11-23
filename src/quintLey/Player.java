5227fe6223d22fdae20ed46e69ddc826e5d89e96
widley95 <widley95@gmail.com>
2017-11-23 12:42:34 -0500

    Initial commit

---
+++ src/quintLey/Player.java
@@ -0,0 +1,20 @@
+package quintLey;
+
+/**
+ * Created by widle on 11/23/2017.
+ */
+public class Player extends AbstractPlayer
+{
+    public Player()
+    {
+        init();
+    }
+    public void init()
+    {
+
+    }
+    public PacFace action(Object state)
+    {
+
+    }
+}
